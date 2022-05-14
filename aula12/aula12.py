8fa532912226a41818a2f35c6bed4b91ea660ab2
Diogo Blanco <diogoblanco@outlook.com.br>
2022-05-14 20:36:17 -0300

    aula 12 e 13

---
+++ aula12/aula12.py
@@ -0,0 +1,16 @@
+"""
+Isso são Docstrings (comentários de bloco)
+Utilizado para documentação
+"""
+
+# Isso é um comentário
+
+print("Linha 1")  # Outro comentário
+print("Linha 2")  # outro
+# Uma Linha
+# Outra Linha
+print("Linha 3")
+print("Linha 4")  # Isso é um comentário
+print("Linha 5")  # mais um
+print("Linha 6")
+# Final
---
+++ aula13/aula13.py
@@ -0,0 +1,15 @@
+from random import randint
+
+# print(123456)
+# print("Diogo", "Blanco", sep="_", end="#####")
+# print("João", "e", "Maria", sep="_", end="")
+
+primeiroBloco = randint(100, 999)
+segundoBloco = randint(100, 999)
+terceiroBloco = randint(100, 999)
+quartoBloco = randint(10, 99)
+
+print(primeiroBloco, end=".")
+print(segundoBloco, end=".")
+print(terceiroBloco, end="-")
+print(quartoBloco)
